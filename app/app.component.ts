534e7950aa55da0ced145e9228cf12efe2c496d4
adonoghuTcd <adonoghu@tcd.ie>
2016-02-17 18:34:36 +0000

    Added angular and typescript to project

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Alan on 16/02/2016.
+ */
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
+
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by Alan on 16/02/2016.
+ */
+
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
