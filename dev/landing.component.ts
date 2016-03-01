f718df4bffdd6286d7728353a15b4abec5fb4e51
jpf8992 <Shogun8992$>
2016-03-01 14:50:31 +0000

    did some debugging with the landing page. Now seems to be working locally for me.

--- dev/landing.component.ts
+++ dev/landing.component.ts
@@ -1,7 +1,7 @@
 /**
  * Created by kfraser on 24/02/2016.
  */
-/// <reference path="../typings/browser/definitions/moment/moment.d.ts" />
+///<reference path="../typings/browser/definitions/moment/moment.d.ts" />
 
 import {Component, View, provide} from 'angular2/core';
 import {Alert} from'ng2-bootstrap/ng2-bootstrap';
