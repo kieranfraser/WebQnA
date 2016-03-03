7e94046a2f0636e8617d1ac2c0268bf823c1bdfb
eoghanmartin <martine4@tcd.ie>
2016-03-03 22:27:05 +0000

    File Additions
    Manually add files with new directories.

---
+++ public/dist/app/question-form.component.js
@@ -0,0 +1,88 @@
+System.register(['angular2/core', 'angular2/http', "./models/question", "./services/http-service", "./dashboard.component"], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var __param = (this && this.__param) || function (paramIndex, decorator) {
+        return function (target, key) { decorator(target, key, paramIndex); }
+    };
+    var core_1, http_1, question_1, http_service_1, dashboard_component_1;
+    var QuestionInputFormComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (question_1_1) {
+                question_1 = question_1_1;
+            },
+            function (http_service_1_1) {
+                http_service_1 = http_service_1_1;
+            },
+            function (dashboard_component_1_1) {
+                dashboard_component_1 = dashboard_component_1_1;
+            }],
+        execute: function() {
+            QuestionInputFormComponent = (function () {
+                function QuestionInputFormComponent(_parent, http, httpService) {
+                    this._parent = _parent;
+                    this.http = http;
+                    this.httpService = httpService;
+                    this.types = ["Free-text", "Multi-choice"];
+                    this.today = new Date();
+                    this.socket = null;
+                    this.questionModel = new question_1.Question(this.selectedClass, "", "", [], [], JSON.parse(localStorage.getItem('profile')).user_id, this.today.toString(), this.types[0], "");
+                    this.submitted = false;
+                    this.selectedClass = this._parent.selectedClass;
+                    this.socket = io('/');
+                    this.socket.on('update', function () {
+                        console.log('Message from server: question feed to be updated');
+                        this._parent.getQuestions();
+                    }.bind(this));
+                }
+                QuestionInputFormComponent.prototype.onSubmit = function () {
+                    this.submitted = true;
+                    this.today = new Date();
+                    console.log(this.questionModel);
+                    console.log(this.selectedClass);
+                    this.questionModel.classid = this.selectedClass;
+                    var json = JSON.stringify(this.questionModel);
+                    console.log(json);
+                    this.httpService.addQuestion(json).subscribe(function (data) { return console.log(JSON.stringify(data)); }, function (error) { return alert(error); }, function () { return console.log("post question success"); });
+                    console.log(JSON.stringify(this.questionModel));
+                    this.questionModel = new question_1.Question(this.selectedClass, "", "", [], [], JSON.parse(localStorage.getItem('profile')).user_id, this.today.toString(), this.types[0], "");
+                    this.socket.emit('update', 'question');
+                    this._parent.isCollapsedQuestion = !this._parent.isCollapsedQuestion;
+                    this._parent.getQuestions();
+                };
+                QuestionInputFormComponent = __decorate([
+                    core_1.Component({
+                        selector: 'question-input-form',
+                        providers: [http_service_1.HTTPService],
+                        inputs: ['selectedClass']
+                    }),
+                    core_1.View({
+                        templateUrl: 'views/question_input_form.html',
+                        directives: []
+                    }),
+                    __param(0, core_1.Inject(core_1.forwardRef(function () { return dashboard_component_1.DashboardComponent; }))), 
+                    __metadata('design:paramtypes', [dashboard_component_1.DashboardComponent, http_1.Http, http_service_1.HTTPService])
+                ], QuestionInputFormComponent);
+                return QuestionInputFormComponent;
+            }());
+            exports_1("QuestionInputFormComponent", QuestionInputFormComponent);
+        }
+    }
+});
+
+//# sourceMappingURL=maps/question-form.component.js.map
