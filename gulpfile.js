308332aa6b1b0feb1ab7f776b3f5a68f93c6caee
adonoghuTcd <adonoghu@tcd.ie>
2016-03-05 23:31:17 +0000

    added minifaction to js and css, requires npm install

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ const tslint = require('gulp-tslint');
 var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon');
 const reload = browserSync.reload;
-
+gp_concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
 const paths = {
     dist: 'public/dist/app',
     distview: 'public/views',
@@ -27,6 +28,10 @@ gulp.task('clean', function () {
 gulp.task('sass-copy:css', function () {
     return gulp.src(paths.srcSass)
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(gp_concat('qanda.css'))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(paths.distStyle));
 });
 // copy static assets - i.e. non TypeScript compiled source
@@ -44,14 +49,32 @@ gulp.task('copy:views', ['clean'], function() {
 // copy dependencies
 gulp.task('copy:libs', ['clean'], function() {
     return gulp.src([
-            'node_modules/angular2/bundles/angular2-polyfills.js',
+            'node_modules/angular2/bundles/angular2-polyfills.min.js',
             'node_modules/systemjs/dist/system.js',
             'node_modules/systemjs/dist/system.js.map',
-            'node_modules/rxjs/bundles/Rx.js',
+            'node_modules/rxjs/bundles/Rx.min.js',
             'node_modules/es6-shim/es6-shim.min.js',
             'node_modules/es6-shim/es6-shim.map',
             'node_modules/systemjs/dist/system-polyfills.js',
             'node_modules/systemjs/dist/system-polyfills.js.map',
+            'node_modules/angular2/bundles/angular2.dev.js',
+            'node_modules/angular2/bundles/router.dev.js',
+            'node_modules/angular2/bundles/http.js',
+            'node_modules/angular2-jwt/angular2-jwt.js',
+            'node_modules/moment/min/moment.min.js',
+            'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.min.js',
+            'node_modules/ng2-charts/**/*'
+        ])
+        .pipe(gulp.dest('public/dist/lib'))
+});
+
+// copy dependencies
+gulp.task('copy:libs-prod', ['clean'], function() {
+    return gulp.src([
+            'node_modules/angular2/bundles/angular2-polyfills.min.js',
+            'node_modules/systemjs/dist/system.js',
+            'node_modules/rxjs/bundles/Rx.js',
+            'node_modules/systemjs/dist/system-polyfills.js',
             'node_modules/angular2/bundles/angular2-polyfills.js',
             'node_modules/rxjs/bundles/Rx.js',
             'node_modules/angular2/bundles/angular2.dev.js',
@@ -64,7 +87,6 @@ gulp.task('copy:libs', ['clean'], function() {
         ])
         .pipe(gulp.dest('public/dist/lib'))
 });
-
 // TypeScript compile
 gulp.task('compile', ['clean'], function () {
     return gulp
